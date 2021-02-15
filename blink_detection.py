fb8c18d8fa53ae0bb1bb9f97e8a9031d9f0b9d1a
YooSunyoug <luysunyoug9@gmail.com>
2021-02-15 17:42:16 +0100

    added image augmentation for brightness

--- blink_detection.py
+++ blink_detection.py
@@ -2,10 +2,14 @@ import tensorflow as tf
 import os
 import numpy as np
 import cv2
-import shutil, random, json
+import random
+import logging
+tf.get_logger().setLevel(logging.ERROR)
 
-# ref: https://github.com/MuhammedBuyukkinaci/TensorFlow-Binary-Image-Classification-using-CNN-s/blob/master/Binary_classification.ipynb
-def preprocess(directory_path="data/pupil_data/mrlEyes_2018_01"):
+def preprocess(directory_path="data/eye_images/mrlEyes_2018_01"):
+    if os.path.exists(directory_path) is False:
+        print("Please download and extract the eye images and check if they are in the right path.")
+        return 0
     dir_list = os.listdir(directory_path)
     image_dictionary = {}
     for dir in dir_list:
@@ -16,13 +20,9 @@ def preprocess(directory_path="data/pupil_data/mrlEyes_2018_01"):
             if int(info[3]) == 1: continue  # if wearing glasses
             if int(info[5]) == 2: continue  # if high reflection
             if int(info[6]) == 0: continue  # if lighting condition in bad
-            if random.random() > 0.4: continue  # 20% sampling
             image_dictionary[os.path.join(dir, image)] = int(info[4])
 
-    print(image_dictionary)
     resized_image_dictionary = {}
-    shutil.rmtree('data/pupil_data')
-    os.mkdir('data/pupil_data')
 
     image_label_list = []
 
@@ -32,27 +32,34 @@ def preprocess(directory_path="data/pupil_data/mrlEyes_2018_01"):
         if img is None: continue
         if img.shape[0] < 50 or img.shape[1] < 50: continue  # discard too small images
         img = cv2.resize(img, (50, 50), interpolation=cv2.INTER_AREA)
-        _, dir, dir2, _ = image.split("/")
-        image = image.replace(dir + '/' + dir2, "pupil_data")
-        cv2.imwrite(image, img)
         resized_image_dictionary[image] = open_closed
         image_label_list.append([img, open_closed])
+        image_label_list.append([np.where(img+img > 255, 255, img+img), open_closed])
+        image_label_list.append([img+img, open_closed])
+        image_label_list.append([img+img*0.5, open_closed])
+        image_label_list.append([img-img*0.1, open_closed])
+        image_label_list.append([img-img*0.2, open_closed])
+        image_label_list.append([img-img*0.3, open_closed])
 
-    with open('data/pupil_data_list.json', 'w') as f:
-        json.dump(resized_image_dictionary, f)
-
-    random.shuffle(image_label_list)
-    train_image_list = image_label_list[:6000]
-    test_image_list = image_label_list[6000:8000]
+    open_eyes = list(filter(lambda x: x[1] == 1, image_label_list))
+    closed_eyes = list(filter(lambda x: x[1] == 0, image_label_list))
+    random.shuffle(open_eyes)
+    random.shuffle(closed_eyes)
+    train_image_list = open_eyes[:45000]
+    train_image_list.extend(closed_eyes[:45000])
+    test_image_list = open_eyes[45000:48000]
+    test_image_list.extend(closed_eyes[45000:48000])
+    random.shuffle(train_image_list)
+    random.shuffle(test_image_list)
 
     train_images = np.array([x[0] for x in train_image_list])
     train_images = train_images / 255
     train_images = train_images.reshape((-1, 50, 50, 1))
     train_labels = np.array([[float(x[1])] for x in train_image_list])
 
-    with open('data/train_image.npy', 'wb') as f:
+    with open('data/eye_images/train_image.npy', 'wb') as f:
         np.save(f, train_images)
-    with open('data/train_label.npy', 'wb') as f:
+    with open('data/eye_images/train_label.npy', 'wb') as f:
         np.save(f, train_labels)
 
     test_images = np.array([x[0] for x in test_image_list])
@@ -60,12 +67,43 @@ def preprocess(directory_path="data/pupil_data/mrlEyes_2018_01"):
     test_images = test_images.reshape((-1, 50, 50, 1))
     test_labels = np.array([[float(x[1])] for x in test_image_list])
 
-    with open('data/test_image.npy', 'wb') as f:
+    with open('data/eye_images/test_image.npy', 'wb') as f:
         np.save(f, test_images)
-    with open('data/test_label.npy', 'wb') as f:
+    with open('data/eye_images/test_label.npy', 'wb') as f:
         np.save(f, test_labels)
 
-def simple_model(X, training=False):
+
+def simple_model():
+    cnn_1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(50, 50, 1))
+    mxp_1 = tf.keras.layers.MaxPooling2D(2, 2)
+    cnn_2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu')
+    mxp_2 = tf.keras.layers.MaxPooling2D(2, 2)
+    cnn_3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')
+    mxp_3 = tf.keras.layers.MaxPooling2D(2, 2)
+    cnn_4 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu')
+    mxp_4 = tf.keras.layers.MaxPooling2D(2, 2)
+    flat = tf.keras.layers.Flatten()
+    dense1 = tf.keras.layers.Dense(512, activation='relu')
+    dense2 = tf.keras.layers.Dense(128, activation='relu')
+    output = tf.keras.layers.Dense(1, activation='sigmoid')
+    model = tf.keras.models.Sequential([
+        cnn_1, mxp_1,
+        cnn_2, mxp_2,
+        cnn_3, mxp_3,
+        #cnn_4, mxp_4,
+        flat,
+        dense1,
+        dense2,
+        output
+    ])
+    model.compile(loss='binary_crossentropy',
+                  optimizer=tf.keras.optimizers.Adam(),
+                  metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0.5)])
+    model.summary()
+    return model
+
+
+def simple_model_tensorflow1(X, training=False):
     nodes_fc1 = 512
     # CONVOLUTION LAYER 1
     # Weights for layer 1
@@ -98,7 +136,7 @@ def simple_model(X, training=False):
     p_2 = tf.nn.max_pool(c_2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
 
     # Flattening
-    flattened = tf.reshape(p_2,[-1,9*9*96])
+    flattened = tf.reshape(p_2, [-1, 9 * 9 * 96])
 
     # Fully Connected Layer 1
     # Getting input nodes in FC layer 1
@@ -127,103 +165,32 @@ def simple_model(X, training=False):
 
 
 def train():
-    train_images = np.load('data/pupil_data/train_image.npy')
-    train_labels = np.load('data/pupil_data/train_label.npy')
+    train_images = np.load('data/eye_images/train_image.npy')
+    train_labels = np.load('data/eye_images/train_label.npy')
+    test_images = np.load('data/eye_images/test_image.npy')
+    test_labels = np.load('data/eye_images/test_label.npy')
 
-    train_size = len(train_images)
-    batch_x, batch_y = None, None
-    learning_rate = 1e-3
-    batch_size = 8
-    epoch = 60
-    save_point = 10
-    checkpoint_dir_path = 'data/'
-    with tf.device('/device:XLA_GPU:0'):
-        with tf.Graph().as_default():
-            X = tf.placeholder(tf.float32, shape=[None, 50, 50, 1], name="normalized_gray_image")
-            y_true = tf.placeholder(tf.float32, shape=[None, 1], name="output")
-            y_pred = simple_model(X, True)
-            # loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_true, logits=y_pred))
-            loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y_true, logits=y_pred))
-            global_step = tf.contrib.framework.get_or_create_global_step()
-            train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=global_step)
-            init = tf.global_variables_initializer()
-            saver = tf.train.Saver()
-            with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
-                sess.run(init)
-                cur_loss = 0
-                for i in range(1, epoch + 1):
-                    batch_epoch_size = int(len(train_images) / batch_size)
-                    for b_num in range(batch_epoch_size):
-                        offset = b_num * batch_size
-                        if offset + batch_size < train_size:
-                            batch_x, batch_y = train_images[offset:(offset + batch_size)], train_labels[
-                                                                                 offset:(offset + batch_size)]
-                        else:
-                            batch_x, batch_y = train_images[offset:], train_labels[offset:]
-                        _, cur_loss = sess.run([train_op, loss],
-                                                       feed_dict={X: batch_x, y_true: batch_y})
-                    print("testing loss : ", i, cur_loss)
-
-                    if i % save_point == 0 or i == epoch:
-                        saver.save(sess, os.path.join(checkpoint_dir_path, "model"), i)
+    model = simple_model()
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath='data/model')
+    model.fit(train_images, train_labels, batch_size=64, epochs=12,
+              validation_data=(test_images, test_labels),
+              callbacks=[cp_callback])
+    model.evaluate(test_images, test_labels)
+    model.save("data/model")
+    del model
 
 
 def test():
-    test_images = np.load('data/pupil_data/test_image.npy')
-    test_labels = np.load('data/pupil_data/test_label.npy')
-    checkpoint_path = 'data/model-70'
+    model = simple_model()
+    test_images = np.load('data/eye_images/test_image.npy')
+    test_labels = np.load('data/eye_images/test_label.npy')
+    model.load_weights('data/model')
+    loss, acc = model.evaluate(test_images, test_labels, verbose=2)
+    print("accuracy : {:5.2f}%".format(100*acc))
 
-    true_positive = 0
-    X = tf.placeholder(tf.float32, [None, 50, 50, 1], name="normalized_gray_image")
-    output_0 = simple_model(X)
-    init = tf.global_variables_initializer()
-    saver = tf.train.Saver()
-    with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
-        sess.run(init)
-        saver.restore(sess, checkpoint_path)
-        for image, label in zip(test_images, test_labels):
-            prediction = sess.run(output_0, feed_dict={X: [image]})
-            print("prediction : ", prediction)
-            print("ground_truth : ", label)
-            pred = round(prediction[0][0])
-            pred = 0 if pred == 0 else 1
-            if pred == round(label[0]): true_positive += 1
-            else:
-                cv2.imshow("test", image)
-                cv2.waitKey(30)
-            image = image
-            message = "c" if round(prediction[0][0]) == 0 else "o"
-            cv2.putText(image, message, (0, 40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
-            message = "c" if label[0] == 0 else "o"
-            cv2.putText(image, message, (0, 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 2)
-
-
-    print("true_positive: ", true_positive)
-
-
-def convert():
-    import tensorflow as tf
-    from tensorflow.python.framework.graph_util import convert_variables_to_constants
-
-    checkpoint_path = "data/model-50"
-    with tf.Graph().as_default():
-        X = tf.placeholder(tf.float32, [None, 50, 50, 1], name="normalized_gray_image")
-        output_0 = simple_model(X, training=False)
-        saver = tf.train.Saver()
-        init = tf.global_variables_initializer()
-        with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
-            sess.run(init)
-            saver.restore(sess, checkpoint_path)
-            minimal_graph = convert_variables_to_constants(sess, sess.graph_def, ["final_output"])
-            tf.io.write_graph(minimal_graph, '.', 'model.pb', as_text=False)
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     # preprocess()
+    # model = simple_model()
     train()
     # test()
-    # convert()
-    with tf.Graph().as_default():
-        X = tf.placeholder(tf.float32, [None, 50, 50, 1], name="normalized_gray_image")
-        s = simple_model(X)
-        print(s)
+
