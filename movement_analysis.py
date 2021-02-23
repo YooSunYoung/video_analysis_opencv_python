bde33fda9592c90e52161ec4422b3efbab09b6f4
YooSunyoug <luysunyoug9@gmail.com>
2021-02-23 14:14:29 +0100

    pupil movement analysis

--- movement_analysis.py
+++ movement_analysis.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+title_font_dict = {'size': 20}
+axis_label_font_dict = {'size': 15}
 
 data = pd.read_csv('result/result.csv', header=0)
 
@@ -11,35 +16,70 @@ data = data.drop(diff[diff['frame_num'] != 1.0].index)
 
 diff['left_distance'] = (diff['left_eye_x'].pow(2) + diff['left_eye_y'].pow(2)) ** 0.5
 diff['right_distance'] = (diff['right_eye_x'].pow(2) + diff['right_eye_y'].pow(2)) ** 0.5
-diff['left_distance'].plot.hist(bins=20, alpha=0.8)
-diff['right_distance'].plot.hist(bins=20, alpha=0.8)
-plt.xlabel('movement distance')
+diff = diff.drop(diff[diff['left_distance'] > 15].index)
+diff = diff.drop(diff[diff['right_distance'] > 15].index)
+diff['left_distance'].plot.hist(bins=15, alpha=0.8)
+diff['right_distance'].plot.hist(bins=15, alpha=0.8)
+plt.yscale('log')
+plt.text(7, 10000, 'distance less than 15')
+plt.text(7, 5000, 'left eye movement mean: {:.3f}'.format(diff['left_distance'].mean()))
+plt.text(7, 2500, 'left eye movement mean: {:.3f}'.format(diff['right_distance'].mean()))
+plt.xlabel('movement distance', fontdict=axis_label_font_dict)
+plt.ylabel('frequency (log scale)', fontdict=axis_label_font_dict)
 plt.legend()
 plt.savefig('result/eyes_movement_distance.png')
+
 
 plt.clf()
 diff['left_eye_x'][:200].plot.line(x=data['frame_num'], figsize=(15, 6))
 diff['right_eye_x'][:200].plot.line(x=data['frame_num'], figsize=(15, 6))
+plt.title('eyes movement on x-axis', fontdict=title_font_dict)
+plt.text(0, 3, 'first 200 frames', bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 10})
+plt.ylabel('eyes movement', fontdict=axis_label_font_dict)
+plt.xlabel('frame number', fontdict=axis_label_font_dict)
 plt.legend()
 plt.savefig('result/eyes_movement_xaxis.png')
 
 plt.clf()
 diff['left_eye_y'][:200].plot.line(x=data['frame_num'], figsize=(15, 6))
 diff['right_eye_y'][:200].plot.line(x=data['frame_num'], figsize=(15, 6))
+plt.title('eyes movement on y-axis', fontdict=title_font_dict)
+plt.text(20, 3, 'first 200 frames', bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 10})
+plt.ylabel('eyes movement', fontdict=axis_label_font_dict)
+plt.xlabel('frame number', fontdict=axis_label_font_dict)
 plt.legend()
 plt.savefig('result/eyes_movement_yaxis.png')
 
 plt.clf()
-plt.hist2d(x=diff['left_eye_x'], y=diff['left_eye_y'])
+plt.hist2d(x=diff['left_eye_x'], y=diff['left_eye_y'], bins=20, norm=mpl.colors.LogNorm())
 plt.colorbar()
+plt.title('left eye movement frequency', fontdict=title_font_dict)
+plt.text(5, 6, 'distance less than 15')
+plt.text(5, 5, 'movement on x-axis mean: {:.5f}'.format(diff['left_eye_x'].mean()))
+plt.text(5, 4, 'movement on y-axis mean: {:.5f}'.format(diff['left_eye_y'].mean()))
+plt.xlabel('movement on x-axis', fontdict=axis_label_font_dict)
+plt.ylabel('movement on y-axis', fontdict=axis_label_font_dict)
 plt.savefig('result/left_eye_movement.png')
 
 plt.clf()
-plt.hist2d(x=diff['right_eye_x'], y=diff['right_eye_y'])
+plt.hist2d(x=diff['right_eye_x'], y=diff['right_eye_y'], bins=20, norm=mpl.colors.LogNorm())
 plt.colorbar()
+plt.title('right eye movement frequency', fontdict=title_font_dict)
+plt.text(-15, 15, 'distance less than 15')
+plt.text(-15, 14, 'movement on x-axis mean: {:.5f}'.format(diff['right_eye_x'].mean()))
+plt.text(-15, 13, 'movement on y-axis mean: {:.5f}'.format(diff['right_eye_y'].mean()))
+plt.xlabel('movement on x-axis', fontdict=axis_label_font_dict)
+plt.ylabel('movement on y-axis', fontdict=axis_label_font_dict)
 plt.savefig('result/right_eye_movement.png')
 
 plt.clf()
-plt.hist2d(x=diff['right_eye_x']+diff['left_eye_x'], y=diff['right_eye_y']+diff['left_eye_y'])
+plt.hist2d(x=pd.concat([diff['right_eye_x'], diff['left_eye_x']]),
+           y=pd.concat([diff['right_eye_y'], diff['left_eye_y']]), bins=20, norm=mpl.colors.LogNorm())
 plt.colorbar()
+plt.title('eyes movement frequency', fontdict=title_font_dict)
+plt.text(-15, 16, 'distance less than 15')
+plt.text(-15, 15, 'movement on x-axis mean: {:.5f}'.format((pd.concat([diff['right_eye_x'], diff['left_eye_x']])).mean()))
+plt.text(-15, 14, 'movement on y-axis mean: {:.5f}'.format((pd.concat([diff['right_eye_y'], diff['left_eye_y']])).mean()))
+plt.xlabel('movement on x-axis', fontdict=axis_label_font_dict)
+plt.ylabel('movement on y-axis', fontdict=axis_label_font_dict)
 plt.savefig('result/both_eyes_movement.png')
