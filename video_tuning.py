dde536ad8f7c424b598f06cef90951f537a14479
Sunyoung Yoo <luysunyoung9@gmail.com>
2021-04-30 19:29:54 +0000

    video metadata with more flexible way

--- video_tuning.py
+++ video_tuning.py
@@ -1,6 +1,8 @@
 import cv2
 import json
+import git
 import sys
+import os
 
 
 def detect_eyes(frame, eye_classifier):
@@ -21,6 +23,8 @@ def brightness_contrast_tuning(frame, brightness=0, contrast=0):
 
 def generate_metadata(video_path, **kwargs):
     capture = cv2.VideoCapture(video_path)
+    if 'original_video_directory' in kwargs.keys():
+        capture = cv2.VideoCapture(os.path.join(kwargs.get('original_video_directory'), video_path))
     ret, frame = None, None
     ret_tmp, frame_tmp = capture.read()
     if ret_tmp is False:
@@ -63,7 +67,14 @@ if __name__ == "__main__":
     arguments = sys.argv
     if len(arguments) > 1:
         video_path = arguments[1]
-    metadata_path = "data/videos/test.json"
-    metadata = generate_metadata(video_path)
+    video_path_splitted = video_path.split('.')
+    video_path_splitted[-1] = "json"
+    json_path = ".".join(video_path_splitted)
+    original_video_directory = "/work/data/"
+    repo = git.Repo('.', search_parent_directories=True)
+    sample_video_directory = repo.working_tree_dir+"/data/"
+    metadata_path = os.path.join(sample_video_directory, json_path)
+    metadata = generate_metadata(video_path, 
+                                 original_video_directory=original_video_directory)
     metadata_file = open(metadata_path, 'w+')
     json.dump(metadata, metadata_file)
